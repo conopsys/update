d7ce67da2eae8e54fb2e734b0c823d118db390dc
conopsys <erlend@prepjam.com>
2025-11-12 11:45:05 +0100

    Add files via upload

---
+++ now.ps1
@@ -0,0 +1,3 @@
+# Show a friendly popup so audience sees a result
+Add-Type -AssemblyName PresentationFramework
+[System.Windows.MessageBox]::Show("Update success! You are now safe `<`3","Conopsys update")
