ee23f926b61c461c811f9cc754acd4ed488f3661
conopsys <erlend@prepjam.com>
2025-11-12 11:44:42 +0100

    Delete critical.ps1

--- critical.ps1
+++
@@ -1,3 +0,0 @@
-# Show a friendly popup so audience sees a result
-Add-Type -AssemblyName PresentationFramework
-[System.Windows.MessageBox]::Show("Update success! You are now safe `<`3","Conopsys update")
